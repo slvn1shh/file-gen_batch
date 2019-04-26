1b84c5ce6dc25739f6a4bc342154b294062f0367
slvn1shh <by.fast1k@gmail.com>
2019-04-26 14:47:48 +0300

    error handling; fixes;

--- SImple_File_Generator.bat
+++ SImple_File_Generator.bat
@@ -9,19 +9,19 @@ color 02
 
 echo.
 echo.
-echo.
+
 
 :drive
-set /p _drive=Set drive you want to use: (C/D/Other) - 
+set /p _drive=Set drive you want to use: (C/D/Other) -
 if exist %_drive%: (
 echo Ok, Current mount catalog is -
-cd %_drive%: 
-goto path ) 
-if "%_drive%" == "" ( if "%_drive%" == "C" goto path
+cd %_drive%:
+goto path )
+if "%_drive%" == "" ( rem if "%_drive%" == "C" goto path
 set "%_drive%" = "C"
 echo Using default drive C
 goto path )
-if not exist %_drive%: ( 
+if not exist %_drive%: (
 cls
 echo Select again, pls!
 goto drive )
@@ -46,16 +46,16 @@ if "%_size%" == "" ( set %_size% = 128 )
 echo.
 
 :create
-@echo creating file with path and name: %_path%\%_name% and size: %_size% bytes 
+@echo creating file with path and name: %_path%\%_name% and size: %_size% bytes
 echo.
 @echo Result:
 
 echo.
-if "%_path%" == "" set _full= %_drive%:\%_name%
-else set _full= %_drive%:\%_path%\%_name%
+if "%_path%" == "" ( set _full= %_drive%:\%_name%
+) else ( set _full= %_drive%:\%_path%\%_name% )
 
 
-IF NOT EXIST %_drive%:%_path% mkdir %_drive%:%_path%
+IF NOT EXIST %_full% mkdir %_drive%:\%_path%
 rem If file is already exist, delete them
 if exist %_full% (
 echo removing old one.. 
